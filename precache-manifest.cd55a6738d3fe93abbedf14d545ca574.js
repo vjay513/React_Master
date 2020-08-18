419309e85177d97e67ace7200a007310db4d744e
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2020-08-18 18:20:52 +0530

    Updates

---
+++ precache-manifest.cd55a6738d3fe93abbedf14d545ca574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3111724b16abb1a32450b06ff56b1b82",
+    "url": "/React_Master/index.html"
+  },
+  {
+    "revision": "460c6e31dcaa0fc36bca",
+    "url": "/React_Master/static/css/main.30828ed4.chunk.css"
+  },
+  {
+    "revision": "e95320c43224b25ac190",
+    "url": "/React_Master/static/js/2.3c02f0db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Master/static/js/2.3c02f0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460c6e31dcaa0fc36bca",
+    "url": "/React_Master/static/js/main.c2196aaf.chunk.js"
+  },
+  {
+    "revision": "d4301b6e7316201d9489",
+    "url": "/React_Master/static/js/runtime-main.4e325486.js"
+  },
+  {
+    "revision": "f1af54fc7d51cc519266d057befb81ab",
+    "url": "/React_Master/static/media/BigelowRules-Regular.f1af54fc.ttf"
+  }
+]);
